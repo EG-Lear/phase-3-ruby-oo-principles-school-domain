0145987b81200f9a65e11a371aa815d2bb9a60b9
Garret <gwiats@gmail.com>
2021-05-27 15:17:53 -0400

    commit tree 6edfcf38d5e54d9c7980c4f7ca04781135396133

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster[grade] != nil
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.each do |key, value|
+      sorted_roster[key] = value.sort
+    end
+    sorted_roster
+  end
+
+end
